14ac3751a13fec1f7f025cbf558917e8ba580e7e
Jorge González <jorge.gonzalez@babel.es>
2016-04-08 12:43:12 +0200

    Fix del apipaths de topic.

--- public/src/values/apipaths.js
+++ public/src/values/apipaths.js
@@ -5,8 +5,8 @@ angular.module("babelreddit").value("apipaths", {
 	users: "/api/v1/users",
 	user: "/api/v1/users/:userid",
 	topics: "/api/v1/topics",
-	topic: "/api/v1/topic/:topicid",
-	posts: "/api/v1/topic/:topidid/posts",
-	post: "/api/v1/topic/:topicid/posts/:postid",
+	topic: "/api/v1/topics/:topicid",
+	posts: "/api/v1/topics/:topidid/posts",
+	post: "/api/v1/topics/:topicid/posts/:postid",
 	comments: "/api/v1/topic/:topicid/posts/:postid/comments"
 })
